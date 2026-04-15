1ff1f869878b539d1630f73af08d0e435aec2135
Tamjid-231 <231612038@student.presidency.edu.bd>
2026-04-15 20:23:27 +0600

    fix path alias for netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
-  plugins: [tanstackStart(), netlify()],
+  plugins: [tsconfigPaths(), tanstackStart(), netlify()],
 });
